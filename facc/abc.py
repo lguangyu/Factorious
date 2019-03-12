fc5fd20f57e7e421115e6b1bf274e3ae1880e43e
Guangyu Li <li.gua@husky.neu.edu>
2019-03-12 12:23:02 -0400

    allowing user-specifiec weights in optimizer

--- facc/abc.py
+++ facc/abc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # these are abstract base classes
 
+import collections as _collections_m_
+
 
 class ProtectedAtrributeHolder(object):
 	"""
@@ -13,3 +15,16 @@ class ProtectedAtrributeHolder(object):
 	# make get attribute does not raise error if not find
 	def __getattr__(self, attr):
 		return vars(self).get(attr, None)
+
+
+class DefaultValueDict(_collections_m_.defaultdict):
+	"""
+	different from collections.defaultdict;
+	if key is missing, return default value but not set it;
+	"""
+	def __missing__(self, key):
+		if not self.default_factory:
+			super(DefaultValueDict, self).__missing__(key)
+		else:
+			self[key] = value = self.default_factory(key)
+			return value
